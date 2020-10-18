6f7a95a3898d9dfb0f2c6303cb8ac13baa86397d
ArthurMelo9 <arthurneuro7@gmail.com>
2020-10-18 10:39:47 +0000

    Logic

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 /**
@@ -7,20 +7,50 @@ import './style.css';
 **/
 
 const Login = (props) => {
+
+    const [username, setUsername] = useState('Arthur');
+    const [password, setPassword] = useState('');
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    const handleUsernameInput = (event) => {
+        setUsername(event.target.value)
+    };
+
+    const handlePasswordInput = (event) => {
+        setPassword(event.target.value)
+    };
+
+    function handleLogin(event) {
+        event.preventDefault();
+        if (username === 'Arthur' && password === '123456')
+            setLoggedIn(true)
+    }
     return (
         <div className="logInBox">
+
+            {
+                loggedIn === true ?
+                    <p>Welcome Arthur! What do you have for us today? Add a new post...</p> : null
+            }
+
+
 
             <form>
                 <span>Login</span>
                 <br />
                 <br />
-                <label><input type="text" placeholder="Username" />Username</label>
+                <label>
+                    <input type="text" value={username} placeholder="Username" onChange={handleUsernameInput} />
+                    Username</label>
                 <br />
                 <br />
-                <label><input type="password" placeholder="Password" /> Password</label>
-                <br />
-                <br />
-                <span>Forgot password? <a href="#">Sign up</a></span>
+                <label><input type="password" value={password} placeholder="Password" onChange={handlePasswordInput} /> Password</label>
+                <br /> <br />
+                <button onClick={handleLogin}>Login</button>
+                <br /> <br />
+                <span><a href="#">Forgot password?</a>
+                    <br />
+                    <a href="#">Sign up</a></span>
             </form>
 
 
